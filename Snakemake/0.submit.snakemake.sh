e58d9e215652e538db569c5d9b40e4b00cbf4ae7
Lim <bellelavie@gmail.com>
2020-06-26 14:19:04 -0400

    shared cluster.yml

--- Snakemake/0.submit.snakemake.sh
+++ Snakemake/0.submit.snakemake.sh
@@ -3,6 +3,7 @@
 nJob=30
 totalWaitTime="48:00"
 timestamp=$(date +%Y%m%d_%H%M%S)
+yml=~/bin/Pipeline/Snakemake/cluster.yml
 
 if [ ! -f diag.pdf ];then
 	module load python3/3.6.3
@@ -17,6 +18,6 @@ bsub -W ${totalWaitTime} -eo bsub.${timestamp}.err -oo bsub.${timestamp}.out \
 	"module load python3/3.6.3
 	snakemake -j $nJob \
 		--latency-wait 60 \
-		--cluster-config cluster.yml \
+		--cluster-config $yml \
 		--cluster 'bsub -W {cluster.walltime} -n {cluster.cpu} -M {cluster.memory} -J $$.{cluster.name} -R {cluster.resource} -eo {cluster.error} -oo {cluster.output}'"
 
