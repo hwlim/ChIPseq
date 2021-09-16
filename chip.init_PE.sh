c9ddcf5a5fd10b8370bf976f13b1aec75aa334f3
Lim <bellelavie@gmail.com>
2021-09-16 10:46:15 -0400

    new: job initiation script

---
+++ chip.init_PE.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [ -z ${COMMON_LIB_BASE+x} ]; then
+	echo -e "Error: LimLabBase is not perperly set up" >&2
+	exit 1
+fi
+source $COMMON_LIB_BASE/commonBash.sh
+
+CHIP_PATH=~/bin/Pipeline/Snamemake.PE
+#if [ -z ${CUTLERY+x} ]; then
+#	echo -e "Error: Environment variable CUTLERY is not defined" >&2
+#	exit 1
+#fi
+
+echo -e "Initializing ChIP-seq:PE analysis" >&2
+
+
+cp -v ${CHIP_PATH}/sample.tsv .
+cp -v ${CHIP_PATH}/Snakefile .
