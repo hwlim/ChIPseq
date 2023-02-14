451401d0e534200b1f1da1e7961b3f6cc4d64665
Lim <bellelavie@gmail.com>
2023-02-14 11:19:16 -0500

    update: skip empty lines

--- chip.make_pool_tsv.sh
+++ chip.make_pool_tsv.sh
@@ -14,5 +14,5 @@ tsv=$1
 assertFileExist $tsv
 
 head -n 1 $tsv
-tail -n +2 $tsv | cut -f 3,7 | sort -k1,1 | uniq |  gawk '{ printf "%s\t%s\t%s\tNULL\tNULL\tNULL\t%s\n", $1, $1, $1, $2 }' 
+tail -n +2 $tsv | grep -v ^$ | cut -f 3,7 | sort -k1,1 | uniq |  gawk '{ printf "%s\t%s\t%s\tNULL\tNULL\tNULL\t%s\n", $1, $1, $1, $2 }' 
 
